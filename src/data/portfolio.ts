982a61a646e9a558b7508924182555d6e77b3c26
jingmikun <jingmikun@gmail.com>
2025-11-05 18:04:36 +0800

    加了一个项目

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -9,5 +9,13 @@ export interface Portfolio {
 
 export const portfolioData: Portfolio[] = [
   // Example entry
-  
+  {
+    title: "Poementer app by Vibe Coding",
+    description:
+      "Construct a platform for poemlovers and potential poets to share their masterpieces and inspirations, this work is empowered by AI",
+    projectUrl: "hhttps://vapour-x.cn/docs/foundation/paper-list/",
+    imageUrl:
+      "/images/poementer.png",
+    codeUrl: "https://github.com/VapourX-ScaleLab/Embodied-Manipulation-Foundation-Model-Paper-List",
+  },
 ];
